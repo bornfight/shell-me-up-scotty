7dfd26e313dc9411d485b57d1703c9aa270d721d
Marko Kruljac <marko.kruljac@degordian.com>
2016-02-26 10:47:53 +0100

    checked off todo

--- hook-me-up.sh
+++ hook-me-up.sh
@@ -58,6 +58,7 @@ brew cask install textwrangler
 echo "You'll need a lot of browsers"
 brew cask install google-chrome firefox
 
+#ToDo I'd like more customizability with the aliases
 echo "Setting some aliases for you. Check them out with 'less ~/.bash_aliases'"
 cp .bash_aliases ~/.bash_aliases
 
